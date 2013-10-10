84aaba372a6413089a7cb138f2927c8f02ea25dd
ebinion <ezekiel@gridshiftdesign.com>
2013-10-09 23:29:37 -0500

    Middlemna configuration done

--- README.markdown
+++
@@ -1,3 +0,0 @@
-# VOXi
-VOXi is a community of people sharing and achieving their dreams while helping others do the same. We want to remove the obstacles that stand between you and your dreams – cost, time, knowledge, and fear.
-
--- config.rb
+++ config.rb
@@ -1,25 +1,47 @@
-# Require any additional compass plugins here.
+###
+# Helpers
+###
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+# Reload the browser automatically whenever files change
+activate :livereload
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Autoprefix CSS
+activate :autoprefixer
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# Directory Indexes
+activate :directory_indexes
 
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+set :build_dir, "tmp"
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
